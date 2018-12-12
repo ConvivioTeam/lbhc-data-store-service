7b3f3a70067663f24bc5abb42d0570bf1bcfc86c
Mike Bell <hello@mikebell.io>
2018-12-12 10:41:16 +0000

    Add contacts table

---
+++ source/database/migrations/2018_12_12_103444_create_contacts.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContacts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('url');
+            $table->string('email');
+            $table->string('name');
+            $table->string('position');
+            $table->string('social_facebook');
+            $table->string('social_twitter');
+            $table->string('phonenumber');
+            $table->datetime('created');
+            $table->datetime('updated');
+            $table->boolean('flagged');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
