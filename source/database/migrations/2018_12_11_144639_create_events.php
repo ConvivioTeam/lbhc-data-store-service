7cb45920c087e3bdce64255b240bdf0898d9dc53
Mike Bell <hello@mikebell.io>
2018-12-12 09:02:38 +0000

    Add events table

---
+++ source/database/migrations/2018_12_11_144639_create_events.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('details');
+            $table->string('time');
+            $table->uuid('venue_id');
+            $table->datetime('created');
+            $table->datetime('updated');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
